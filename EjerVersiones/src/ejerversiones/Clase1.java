c60033b725a0c05d9d46964bc39a71b5a7eafee2
dam <dam@172.26.43.178>
2020-01-29 11:54:40 +0100

    Segundo

---
+++ EjerVersiones/src/ejerversiones/Clase1.java
@@ -0,0 +1,17 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package ejerversiones;
+
+/**
+ *
+ * @author dam
+ */
+public class Clase1 {
+    String frase="hola";
+    int examen=2;
+    
+    
+}
